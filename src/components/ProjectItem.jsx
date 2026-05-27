0d48a952e2e948b4eb3b570d362c97130d48f8c9
ratmansky <v.ratmansky@gmail.com>
2026-05-27 17:33:08 +0200

    Make mobile project previews clickable

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -29,6 +29,26 @@ export default function ProjectItem({ project, onPreviewShow }) {
 
   const linkBody = (
     <>
+      <figure
+        className={[
+          'project-item-inline-preview',
+          project.previewImage ? 'project-item-inline-preview--image' : '',
+        ]
+          .filter(Boolean)
+          .join(' ')}
+        style={{ '--project-preview-color': project.previewColor }}
+        aria-hidden="true"
+      >
+        {project.previewImage ? (
+          <img
+            className="project-item-inline-preview-image"
+            src={project.previewImage}
+            alt=""
+            loading="lazy"
+            decoding="async"
+          />
+        ) : null}
+      </figure>
       <div className="project-header">
         <span className="project-title hover-link">{project.title}</span>
         <span className="project-date">{project.period}</span>
@@ -45,27 +65,6 @@ export default function ProjectItem({ project, onPreviewShow }) {
         onPointerMove={handlePreviewPointer}
         onFocus={() => onPreviewShow(project)}
       >
-        <figure
-          className={[
-            'project-item-inline-preview',
-            project.previewImage ? 'project-item-inline-preview--image' : '',
-          ]
-            .filter(Boolean)
-            .join(' ')}
-          style={{ '--project-preview-color': project.previewColor }}
-          aria-hidden="true"
-        >
-          {project.previewImage ? (
-            <img
-              className="project-item-inline-preview-image"
-              src={project.previewImage}
-              alt=""
-              loading="lazy"
-              decoding="async"
-            />
-          ) : null}
-        </figure>
-
         {isRouterLink ? (
           <Link className="project-item-link" to={project.to} onClick={onClick}>
             {linkBody}
